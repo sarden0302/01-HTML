e7b2dd8d048970f6622bf4c77ebf4fe712a30483
sarden0302 <sarden030@gmail.com>
2024-11-16 21:20:19 +0900

    241116 24115 과제 final

--- 실습과제/241115/script.js
+++ 실습과제/241115/script.js
@@ -1,39 +1,43 @@
 const agreement = document.getElementsByName("agreement");
-const button = document.getElementById("button");
 function buttonActive() {
-    button.disabled = false;
+    buttonAgreement.disabled = false;
 }
 
-button.addEventListener('click', () => {
+function buttonAgreement(){
     for (let i = 0; i < agreement.length; i++) {
         if (!agreement[i].checked) {
             alert("모든 필수 약관에 동의해야 합니다.");
-            button.disabled = true;
+            buttonAgreement.disabled = true;
             return;
         }
     }
-});
-
-//1. 가입 버튼 성공시 성공.html로 이동
-//2. addeventlistener 사용
+};
 
 const idRegex = /^[A-z0-9]{5, 12}$/;
+const buttonSignup = document.getElementById("buttonSignup");
 
-function checkingSignup() {
+buttonSignup.addEventListener('click',() =>{
     const userId = document.getElementById("user-id");
     const userPw = document.getElementById("user-pw");
     const userEmail = document.getElementById("user-email");
 
     console.log("checkingSignup 실행")
-    if (!checkingId(userId)) {
+    if (!checkingId(userId.value)) {
         console.log("checkingId 실행");
         alert("아이디는 5~12자의 영문자 및 숫자여야 합니다.");
         return;
     }
-}
-
+    window.location.href = "finalPage.html";
+    alert("로그인 성공");
+});
 function checkingId(userId) {
-    if (userId.value.trim().length == 0) return false;
-    if (!idRegex.test(userId.value)) return false;
+    if (userId.trim().length == 0) {
+        console.log("trim");
+        return false;
+    }
+    if (idRegex.test(userId)) {
+        console.log("test");
+        return false;
+    }
     return true;
 }
